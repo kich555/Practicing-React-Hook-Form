f419e255348530c754611611f83532fe2c2b6624
kich555 <hyunjjang199582@gmail.com>
2022-06-20 16:59:45 +0900

    Add ErrorMessage component & handle error on AntD input

--- src/AndtD.js
+++ src/AndtD.js
@@ -1,15 +1,43 @@
 import * as React from 'react';
 import { Controller } from 'react-hook-form';
 import { Input, Select, Checkbox as AntdCheckbox, Switch as AntdSwitch, Slider as AntdSlider, Radio } from 'antd';
+import { ErrorMessage } from '@hookform/error-message';
 
 const { Option } = Select;
-
-export default function AntD({ control }) {
+const { TextArea } = Input;
+export default function AntD({ control, errors }) {
   return (
     <div className="container">
       <section>
         <label>Antd Input</label>
         <Controller placeholder="AntD Input" control={control} name="AntdInput" render={({ field }) => <Input {...field} />} />
+        <ErrorMessage errors={errors} name="AntdInput" message="This is required" />
+      </section>
+      <section>
+        <label>Antd Textarea</label>
+        <Controller
+          placeholder="AntD Input"
+          control={control}
+          name="AntdTextArea"
+          rules={{
+            required: { value: true, message: 'This is required??' },
+            maxLength: {
+              value: 10,
+              message: 'AntdTextArea must be at least 10 characters long.',
+            },
+            // maxLength: {
+            //   value: 10,
+            //   message: 'AntdTextArea must be at least 10 characters long.',
+            // },
+          }}
+          render={({ field }) => <TextArea {...field} />}
+        />
+        <ErrorMessage errors={errors} name="AntdTextArea">
+          {({ messages }) => {
+            console.log(messages);
+            return messages && Object.entries(messages).map(([type, message]) => <p key={type}>{message}</p>);
+          }}
+        </ErrorMessage>
       </section>
       <section>
         <label>Antd Checkbox</label>
